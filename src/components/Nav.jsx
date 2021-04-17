5f48bd594e5a044072e0199ff9677a076e7d007e
kehinde <bankolek1@gmail.com>
2021-04-17 13:03:52 +0100

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/Image.png'
+import './Nav.css'
+function Nav() {
+    return (
+        <div id="nav">
+            <img src={logo} />
+        </div>
+    )
+}
+
+export default Nav
